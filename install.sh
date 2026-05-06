ce47d27614969562b332d01b4306cfe58d2eead6
David Gent <davidgent@notsourgent.com>
2026-05-05 22:46:43 -0700

    Install script additions -- still needs testing and user input before it can be used.  See the source for details.

--- install.sh
+++ install.sh
@@ -6,12 +6,15 @@
 # ALPHA -- probably not really useful
 #
 
+echo "Aborting -- this is an alpha script, and needs some testing and user input before it can be used.  See the source for details."
 exit 0
 
 fatal () {
     echo "[FATAL, $LINENO] $@" >$2
     exit 1
 }
+
+## Check for files and commands we need
 file=( OpenURLwith OpenURLwith.desktop )
 cmds=( update-desktop-database xdg-settings )
 
@@ -23,15 +26,23 @@ for cmd in "${cmds[@]}" ; do
     [ $(type -t $cmd) = "file" ] || fatal "$cmd missing"
 done
 
+
 echo "Copying script to $HOME/bin"
 cp OpenURLwith $HOME/bin || fatal 
 chmod 755 $HOME/bin/OpenURLwith || fatal
 ls -l $HOME/bin/OpenURLwith
 
+echo "Running to see if it works"
+OpenURLwith https://www.google.com || fatal "Script doesn't work"
+
 echo "Installing desktop file"
 cp OpenURLwith.desktop $HOME/.local/share/applications || fatal
 chmod 755 $HOME/.local/share/applications/OpenURLwith.desktop || fatal
 ls -l $HOME/.local/share/applications/OpenURLwith.desktop  || fatal
+
+echo "Running `gio launch $HOME/.local/share/applications/OpenURLwith.desktop` to see if it works"
+echo "This may prompt your desktop environment to ask you to trust the file. Please click 'Trust and Launch' or similar."
+gio launch $HOME/.local/share/applications/OpenURLwith.desktop
 
 update-desktop-database ~/.local/share/applications || fatal 
 
